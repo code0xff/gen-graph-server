26cc6cbf7cccd493969b4f0e38879a176776b43b
Ian J.Um <31336310+code0xff@users.noreply.github.com>
2018-12-11 00:17:47 +0900

    edit upload file

--- routes/assets.js
+++ routes/assets.js
@@ -1,27 +1,34 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-const multer = require('multer');
-const upload = multer({dest: 'public/images/'});
+var multer = require('multer');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+var upload = multer({ storage: storage });
 
 router.get('/', function(req, res, next) {
   let jsonData = fs.readFileSync(__dirname + '/../storages/assets.json', 'utf8');
   let dataObject = JSON.parse(jsonData);
-  
+
   res.send(dataObject);
 });
 
 router.post('/', upload.single('assetFile'), function (req, res) {
-  console.log('filename: ' + req.file.filename);
-  console.log('path: ' + req.file.path);
-  console.log('originalname: ' + req.file.originalname);
-
   console.log('assetName: ' + req.body.assetName);
 
   let jsonData = fs.readFileSync(__dirname + '/../storages/assets.json', 'utf8');
   let assets = JSON.parse(jsonData);
   let mapSetList =  assets.mapSetList;
-  mapSetList[req.body.assetName] = req.file.filename;
+  let fileName = req.file.filename;
+  let dotIndex = fileName.indexOf('.');
+  fileName = fileName.slice(0, dotIndex);
+  mapSetList[req.body.assetName] = fileName;
 
   let mapList = assets.mapList;
   mapList.push(req.body.assetName);
